c07544e162f6a5d24364d8459dce4ef0eb106182
Jatin Mehta <jatinmehta.jm@gmail.com>
2022-08-14 16:43:22 -0400

    Parse done

--- main.js
+++ main.js
@@ -11,49 +11,15 @@ const alchemy = new Alchemy(config);
 
 const cryptoCompareAPIKey = "318137a74abd9b3d645c6f73753fcec5378cbc367dd0b94f0147be390bd3f4f7";
 
+const cryptoSymbol = 'ETH'
 
-const response = await fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH&tsyms=USD&api_key='+cryptoCompareAPIKey);
+const response = await fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms='+cryptoSymbol+'&tsyms=USD&api_key='+cryptoCompareAPIKey);
 const data = await response.json();
 
-console.log("CC RESPONSE:    " + JSON.stringify(data));
+console.log("CC RESPONSE:    " + data[cryptoSymbol]["USD"]);
 
-// var options = {
-//   "method": "GET",
-//   "hostname": "rest.coinapi.io",
-//   "path": "/v1/exchangerate/BTC?invert=false",
-//   "headers": {'X-CoinAPI-Key': '56A2C0BC-181A-4DD7-8FC3-697B3C2EBD21'}
-// };
-//
-// var request = https.request(options, function (response) {
-//   var chunks = [];
-//   response.on("data", function (chunk) {
-//     chunks.push(chunk);
-//   });
-// });
-//
-// console.log("RESPONSE:    " + JSON.stringify(request));
-// request.end();
-
-// const messariApiKey = '714fc350-9b4c-4e0c-bd79-916849342ec3'
-// const response = https
-//     .request(
-//         {
-//           host: "data.messari.io",
-//           path: "/api/v1/assets/btc",
-//           headers: { "x-messari-api-key": messariApiKey },
-//         },
-//         function (response) {
-//           let str = "";
-//           response.on("data", (chunk) => (str += chunk));
-//           response.on("end", () => console.log(JSON.parse(str)));
-//         }
-//     )
-//     .end();
-//
-// console.log("Messari RESPONSE:    " + JSON.stringify(response));
-
-
-
+// this works
+// console.log("CC RESPONSE:    " + data.ETH.USD);
 
 const main = async () => {
   // Wallet address
