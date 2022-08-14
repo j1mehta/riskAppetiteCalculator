aa097e66c33c29c2148e43d2618293cefea12ca6
Jatin Mehta <jatinmehta.jm@gmail.com>
2022-08-14 15:45:46 -0400

    Price fetching works

--- main.js
+++ main.js
@@ -1,11 +1,59 @@
 // Setup: npm install alchemy-sdk
 import { Alchemy, Network } from "alchemy-sdk";
+import fetch from 'node-fetch';
+import * as https from 'https';
 
 const config = {
-  apiKey: "ALCHEMY_API_KEY",
+  apiKey: "mZ_9bUQbJiMBHbX-oRYGQz4nL4ff2M0H",
   network: Network.ETH_MAINNET,
 };
 const alchemy = new Alchemy(config);
+
+const cryptoCompareAPIKey = "318137a74abd9b3d645c6f73753fcec5378cbc367dd0b94f0147be390bd3f4f7";
+
+
+const response = await fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH&tsyms=USD&api_key='+cryptoCompareAPIKey);
+const data = await response.json();
+
+console.log("CC RESPONSE:    " + JSON.stringify(data));
+
+// var options = {
+//   "method": "GET",
+//   "hostname": "rest.coinapi.io",
+//   "path": "/v1/exchangerate/BTC?invert=false",
+//   "headers": {'X-CoinAPI-Key': '56A2C0BC-181A-4DD7-8FC3-697B3C2EBD21'}
+// };
+//
+// var request = https.request(options, function (response) {
+//   var chunks = [];
+//   response.on("data", function (chunk) {
+//     chunks.push(chunk);
+//   });
+// });
+//
+// console.log("RESPONSE:    " + JSON.stringify(request));
+// request.end();
+
+// const messariApiKey = '714fc350-9b4c-4e0c-bd79-916849342ec3'
+// const response = https
+//     .request(
+//         {
+//           host: "data.messari.io",
+//           path: "/api/v1/assets/btc",
+//           headers: { "x-messari-api-key": messariApiKey },
+//         },
+//         function (response) {
+//           let str = "";
+//           response.on("data", (chunk) => (str += chunk));
+//           response.on("end", () => console.log(JSON.parse(str)));
+//         }
+//     )
+//     .end();
+//
+// console.log("Messari RESPONSE:    " + JSON.stringify(response));
+
+
+
 
 const main = async () => {
   // Wallet address
@@ -23,7 +71,7 @@ const main = async () => {
 
   // Counter for SNo of final output
   let i = 1;
-  totalBalance = 
+
 
   // Loop through all tokens with non-zero balance
   for (let token of nonZeroBalances) {
